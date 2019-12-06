af1b02cd6346898b7ec15ef1dcf5a5c366aebc34
satoshi-r <rgeargerge@yandex.ru>
2019-12-06 05:13:57 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,151 +1,125 @@
-// Запрет скролла при загрузке
-$('html, body').css({ 'overflow': 'hidden' });
-
-$(document).ready(function(){
-
-	// Подсказка
-	$(function() {
-		$('.ring').tooltip('hide');
+$(document).ready(function () {
+	// прокрутка по якорям
+	$(function () {
+		$("#first-arrow-js").click(function () {
+			$("html, body").animate({ scrollTop: $('#advantages').offset().top + "px" });
+			return false;
+		});
 	});
 
 	// Слайдер
 	$('.single-item').slick({
-		dots:true,
-		autoplay:false,
-		centerMode:false,
+		dots: true,
+		autoplay: false,
+		centerMode: false,
 		infinite: true,
 		// centerPadding:60,
-		arrows:true,
+		arrows: true,
 		responsive:
-		[
-			{
-				breakpoint: 992,
-				settings:{
-					centerMode: true
-			    }
-			},
-			{
-				breakpoint: 860,
-				settings:{
-					centerMode: true,
-
-			    }
-			},
-			{
-				breakpoint: 768,
-					settings:{
+			[
+				{
+					breakpoint: 992,
+					settings: {
+						centerMode: true
+					}
+				},
+				{
+					breakpoint: 860,
+					settings: {
 						centerMode: true,
-						autoplay:false,
+
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						centerMode: true,
+						autoplay: false,
 						arrows: false
 					}
-			}
-		]
+				}
+			]
 	});
 	// Плавная прокрутка в Хроме
 	try {
 		$.browserSelector();
-		if($("html").hasClass("chrome")) {
+		if ($("html").hasClass("chrome")) {
 			$.smoothScroll();
 		}
-	} catch(err) {
+	} catch (err) {
 
 	};
 
 	// Запрет на вытаскивание картинок
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	$("img, a").on("dragstart", function (event) { event.preventDefault(); });
 
 	// Вернуться назад
-	$(function (){
+	$(function () {
 		$(".back-top").hide();
 
-		$(window).scroll(function (){
-			if ($(this).scrollTop() > 700){
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 700) {
 				$(".back-top").fadeIn();
-			} else{
+			} else {
 				$(".back-top").fadeOut();
 			}
 		});
 
-		$(".back-top").click(function (){
+		$(".back-top").click(function () {
 			$("body,html").animate({
-				scrollTop:0
+				scrollTop: 0
 			}, 800);
 			return false;
 		});
 	});
 
 	// Модальное окно
-	$(".ring, footer_ring").click(function(){
-  	$('#exampleModal').arcticmodal();
+	$(".ring, footer_ring").click(function () {
+		$('#exampleModal').arcticmodal();
 	});
 
 	// Поправка адаптивности
-	if($(window).width() < 992)
-	{
+	if ($(window).width() < 992) {
 		$('.competence').removeClass('advanteges_width');
 	}
 
-	// Анимация
-	$(window).scroll(function (){
-		if ($(this).scrollTop() > 300){
+	// Анимации
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
 			$('.reliability, .competence, .authenticity').removeClass('hidden');
 			$('.reliability').addClass('fadeInLeft');
 			$('.competence').addClass('fadeInUp');
 			$('.authenticity').addClass('fadeInRight');
 		}
 	});
-	$(window).scroll(function (){
-		if ($(this).scrollTop() > 900){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 900) {
 			$('.relocation_box > img').addClass('swing');
 		}
 	});
-	$(window).scroll(function (){
-		if ($(this).scrollTop() > 1700){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1700) {
 			$('.img1, .img2, .img3').removeClass('hidden');
 			$('.img1, .img2, .img3').addClass('fadeIn');
 		}
 	});
 });
 
-
-// Возвращение скролла после загрузки страницы
-$(document).ready(function(){
-	setTimeout(function () {
-		$('html, body').css({ 'overflow': 'auto' });
-	}, 2500);
-}, 2500);
-
-// Прелоадер
-var hellopreloader = document.getElementById("hellopreloader_preload");
-
-function fadeOutnojquery(el) {
-    el.style.opacity = 1;
-
-    var interhellopreloader = setInterval(function() {
-        el.style.opacity = el.style.opacity - 0.05;
-        if (el.style.opacity <= 0.05) {
-            clearInterval(interhellopreloader);
-            hellopreloader.style.display = "none";
-        }
-    }, 16);
-}
-window.onload = function() {
-    setTimeout(function() {
-        fadeOutnojquery(hellopreloader);
-    }, 1000);
-};
-
 // Маска телефона
-jQuery(function($) {
+jQuery(function ($) {
 	$('#form_tel').mask("+7(999)-999-99-99");
 });
 
 // Бургер
-function hold(){
-document.getElementById('leftdrop').style.display = 'block';
-document.getElementById('leftdrop').style.animationName = 'leftdrop_in';
-};
+$('#burger-open-js').click(function () {
+	$('#leftdrop').css({
+		'display': 'block',
+		'animation-name': 'leftdrop_in'
+	})
+});
 
-function times(){
-	document.getElementById('leftdrop').style.animationName = 'leftdrop_out';
-};
+$('#close').click(function () {
+	$('#leftdrop').css({
+		'animation-name': 'leftdrop_out'
+	})
+});
